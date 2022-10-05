eba27c1309b3934c92f8627c870c6c1d8b2fc21b
Nbiba Bedis <bedisnbiba@gmail.com>
2022-10-05 17:56:40 +0100

    Add parallelMap

--- src/utils/array.ts
+++ src/utils/array.ts
@@ -1,8 +1,27 @@
 declare global {
   interface Array<T> {
     filterMap<E>(f: (x: T) => E | undefined): E[];
+    parallelMap<U>(
+      callbackfn: (value: T, index: number, array: T[]) => Promise<U>,
+      // deno-lint-ignore no-explicit-any
+      thisArg?: any,
+    ): Promise<U[]>;
   }
 }
+
+Array.prototype.parallelMap = async function <T, U>(
+  callbackfn: (value: T, index: number, array: T[]) => Promise<U>,
+  // deno-lint-ignore no-explicit-any
+  thisArg?: any,
+): Promise<U[]> {
+  const promises = [];
+  const target = thisArg !== undefined ? thisArg : this;
+  for (let i = 0; i < target.length; i++) {
+    promises.push(callbackfn(target[i], i, target));
+  }
+  return await Promise.all(promises);
+};
+
 Array.prototype.filterMap = function <T, E>(f: (x: T) => E | undefined): E[] {
   return this.map(f).filter((x) => x) as E[];
 };
---
+++ src/utils/array_test.ts
@@ -0,0 +1,10 @@
+import { assertEquals } from "https://deno.land/std@0.130.0/testing/asserts.ts";
+import { } from "./array.ts"
+
+Deno.test("parallelMap", async () => {
+  assertEquals(
+    // deno-lint-ignore require-await
+    await ["a", "b", "c"].parallelMap(async (e) => e + "1"),
+    ["a1", "b1", "c1"],
+  );
+});
